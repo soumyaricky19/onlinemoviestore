1cf221bb8a8d86cfef16f7e02833c87c5ab32684
Soumya Mishra <soumya.ricky19@gmail.com>
2017-10-28 21:51:47 -0600

    tokenization done

--- search_result.php
+++ search_result.php
@@ -10,7 +10,7 @@
       <nav class='navbar navbar-inverse' style='background-color: rgba(10, 10, 10, 1); margin:0%;'>
           <div class='container-fluid'>
             <ul class='nav navbar-nav'>
-                <li class='active'><a href='search_result.php'>Home</a></li> 
+                <li class='active'><a href='home.html'>Home</a></li> 
                 <li><a href='/checkin' style='height: 10px'>Login</a></li>
                 <li><a href='/borrower' style='height: 10px'>Sign Up</a></li>
                 <li><a href='/fines' style='height: 10px'>Cart</a></li>
@@ -26,6 +26,7 @@
         <br/> 
       <div>
             <?php
+                $stop_list=array(",",";",":","a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves");
                 $search_text = $_GET["search"];
                 $servername = "localhost";
                 $username = "root";
@@ -37,14 +38,44 @@
                 if (!$conn) {
                     die("Connection failed: " . mysqli_connect_error());
                 }
-                $query="select * from movie where title like '%".$search_text."%'";
-                $result = mysqli_query($conn,$query);
-                $list="<ul>";
-                while ($row = mysqli_fetch_array($result)){
-                    $list=$list."<li>".$row["title"]."</li>";
+                $tokens=tokenize($search_text);
+                
+                foreach($tokens as $token) {
+                    if (!in_array($token,$stop_list))
+                    {
+                        //Title search 
+                        $query="select * from movie where title like '%".$token."%'";
+                        display($conn,$query);
+
+                         //Director search
+                        $query="select * from movie where director like '%".$token."%'";
+                        display($conn,$query);
+
+                        //Actor search
+                        //$query="select * from movie where title like '%".$search_text."%'";
+                        //display($conn,$query);
+                    }
                 }
-                $list=$list."</ul>";
-                echo $list;
+                
+               
+
+                
+                
+                function display($conn,$query) {
+                    $result = mysqli_query($conn,$query);
+                    $list="<ul>";
+                    while ($row = mysqli_fetch_array($result)){
+                        $list=$list."<li>".$row["title"]."</li>";
+                    }
+                    $list=$list."</ul>";
+                    echo $list;
+                }
+                // function tokenize($search_text)
+                function tokenize($search_text)
+                {
+                    $tokens = explode(" ",$search_text); 
+                    return $tokens;
+                }
             ?>
         </div>  
         <h1>Recent</h1>
